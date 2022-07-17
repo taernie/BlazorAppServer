9dc335b7e3c7339a2cec62cf430e87a852c96a9f
Ernie Manayon <manayonern@gmail.com>
2022-07-17 21:54:00 +1200

    saved

---
+++ InsideEdgeModels/BlazorModels/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsideEdgeModels.BlazorModels
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; } = string.Empty;
+
+        public int Quantity{ get; set; }
+
+        public double Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public string? Body { get; set; }
+        public string? Neck { get; set; }
+        public string? Color { get; set; }
+
+    }
+}
